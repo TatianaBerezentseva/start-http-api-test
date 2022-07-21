179b725c6aa696074056cfba32477c040fc2fbb2
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-07-21 18:57:38 +0300

    Init commit

---
+++ jest.local.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testMatch: ['**/specs/**/*.spec.*'],
+  testRunner: 'jest-jasmine2',
+  setupFilesAfterEnv: ['jest-allure/dist/setup'],
+  globals: {
+    testTimeout: 50000,
+  },
+  verbose: true,
+};
---
+++ specs/user/index.spec.js
@@ -0,0 +1,43 @@
+import supertest from "supertest";
+
+describe('user', () => {
+  describe('POST /api/v1/login', () => {
+    test('Метод должен существовать', async () => {
+      const res = await supertest('https://try.vikunja.io')
+        .post('/api/v1/login')
+        .send({})
+
+      expect(res.status).not.toEqual(404);
+    })
+
+    test('Авторизация должна проходить успешно с правильным логином и паролем', async () => {
+      const res = await supertest('https://try.vikunja.io')
+        .post('/api/v1/login')
+        .set('Accept', 'application/json')
+        .send({ username: 'demo', password: 'demo' })
+
+      expect(res.status).toEqual(200);
+      expect(typeof res.body.token).toEqual('string')
+    })
+
+    test('Авторизация должна возвращать статус с кодом ошибки если логин неверный', async () => {
+      const res = await supertest('https://try.vikunja.io')
+        .post('/api/v1/login')
+        .set('Accept', 'application/json')
+        .send({ username: 'demo1', password: 'demo' })
+
+      expect(res.status).toEqual(412);
+      expect(res.body.code).toEqual(1011)
+    })
+
+    test('Авторизация должна возвращать статус с кодом ошибки если пароль неверный', async () => {
+      const res = await supertest('https://try.vikunja.io')
+        .post('/api/v1/login')
+        .set('Accept', 'application/json')
+        .send({ username: 'demo', password: 'demo1' })
+
+      expect(res.status).toEqual(412);
+      expect(res.body.code).toEqual(1011)
+    })
+  })
+})
