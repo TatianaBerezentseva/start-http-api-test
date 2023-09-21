9223c4d0ad5ef8ea3863591a990b125a28638e03
Kim Natalia Valentinovna <nkim@ozon.ru>
2023-09-22 01:15:27 +0300

    http tests

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,44 +1,61 @@
-import supertest from "supertest";
+import fetch from 'node-fetch'
+import { expect, test } from '@jest/globals'
 
-describe('user', () => {
-  describe('POST /api/v1/login', () => {
-    test('Метод должен существовать', async () => {
-      const res = await supertest('https://try.vikunja.io')
-          .post('/api/v1/login')
-          .send({})
+const postMethod = 'POST'
+const contentTypeHeader = { 'Content-type': 'application/json' }
 
-      expect(res.status).not.toEqual(404);
-    })
-
-    test('Авторизация должна проходить успешно с правильным логином и паролем', async () => {
-      const res = await supertest('https://try.vikunja.io')
-          .post('/api/v1/login')
-          .set('Accept', 'application/json')
-          .send({username: 'demo', password: 'demo'})
-
-      expect(res.status).toEqual(200);
-      expect(typeof res.body.token).toEqual('string')
-    })
-
-    test('Авторизация должна возвращать статус с кодом ошибки если логин неверный', async () => {
-      const res = await supertest('https://try.vikunja.io')
-          .post('/api/v1/login')
-          .set('Accept', 'application/json')
-          .send({username: 'demo4', password: 'demo'})
-
-      expect(res.status).toEqual(412);
-      expect(res.body.code).toEqual(1011)
-    })
-
-    test('Авторизация должна возвращать статус с кодом ошибки если пароль неверный', async () => {
-      const res = await supertest('https://try.vikunja.io')
-          .post('/api/v1/login')
-          .set('Accept', 'application/json')
-          .send({username: 'demo', password: 'demo3'})
-
-
-      expect(res.status).toEqual(412);
-      expect(res.body.code).toEqual(1011)
-    })
+test('Создание пользователя c ошибкой: логин уже используется', async () => {
+  const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+    method: postMethod,
+    body: JSON.stringify({ userName: 'Loo_ZU', password: 'Loo_ZU_123!' }),
+    headers: contentTypeHeader
   })
+  const result = await response.json()
+  expect(response.status).toEqual(406)
+  expect(result.message).toEqual('User exists!')
+})
+
+test('Создание пользователя c ошибкой: пароль не подходит', async () => {
+  const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+    method: postMethod,
+    body: JSON.stringify({ userName: 'Loo', password: 'loo_zo' }),
+    headers: contentTypeHeader
+  })
+  const result = await response.json()
+  expect(response.status).toEqual(400)
+  expect(result.message).toEqual("Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.")
+})
+
+test('Успешное создание пользователя', async () => {
+  const response = await fetch('https://bookstore.demoqa.com/Account/v1/User', {
+    method: postMethod,
+    body: JSON.stringify({ userName: 'Simply_Redy', password: 'Simply_Redy_123!' }),
+    headers: contentTypeHeader
+  })
+  expect(response.status).toEqual(201)
+})
+
+test('Генерация токена с ошибкой (некорректные логин и пароль)', async () => {
+  const response = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
+    method: postMethod,
+    body: JSON.stringify({ userName: '', password: 12345 }),
+    headers: contentTypeHeader
+  })
+  const result = await response.json()
+  expect(response.status).toEqual(400)
+  expect(result.message).toEqual('UserName and Password required.')
+})
+
+test('Успешная генерация токена для существующего юзера', async () => {
+  const response = await fetch('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
+    method: postMethod,
+    body: JSON.stringify({ userName: 'Simply_Red', password: 'Simply_Red_123!' }),
+    headers: contentTypeHeader
+  })
+  const responseBody = await response.json()
+  expect(response.status).toEqual(200)
+  expect(responseBody.token).toBeTruthy()
+  expect(responseBody.expires).toBeTruthy()
+  expect(responseBody.status).toEqual('Success')
+  expect(responseBody.result).toEqual('User authorized successfully.')
 })
