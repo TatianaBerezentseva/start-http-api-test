417c3c69d969b2d738b50570a2ab1e658b742204
kosenko.dev <d.kosenko@qiwi.com>
2023-10-07 22:07:20 +0300

    #7 Библиотеки для тестирования API

---
+++ framework/services/user.js
@@ -0,0 +1,36 @@
+import supertest from "supertest";
+
+import config from "../config/config";
+const { faker } = require('@faker-js/faker');
+const {url} = config
+
+// контроллер user
+const user = {
+    // Функция авторизации
+    create: (payload) => {
+        return supertest(url)
+            .post('/Account/v1/User')
+            .set('Accept', 'application/json')
+            .send(payload)
+    },
+
+    delete: (uuid) => {
+        return supertest(url)
+            .delete(`/Account/v1/User/${uuid}`)
+            .set('Accept', 'application/json')
+            .send()
+    },
+
+    get: (uuid) => {
+        return supertest(url)
+            .get(`/Account/v1/User/${uuid}`)
+            .set('Accept', 'application/json')
+            .send()
+    },
+
+    randomUserName() {
+        return `${faker.person.lastName()}-${faker.person.firstName()}`;
+    }
+}
+
+export default user
---
+++ specs/user-post.spec.js
@@ -0,0 +1,38 @@
+import supertest from "supertest";
+import config from "../framework/config/config";
+import user from "../framework/services/user";
+
+describe('User', () => {
+  describe('POST /Account/v1/User', () => {
+    test('Метод должен существовать', async () => {
+      const res = await supertest(config.url)
+          .post('/Account/v1/User')
+          .send({})
+
+      expect(res.status).not.toEqual(404);
+    })
+
+    test('Создание нового пользователя парой userName и password', async () => {
+      const res = await user.create({ "userName" : user.randomUserName(), "password": `${config.defaultPassword}` })
+
+      expect(res.status).toEqual(201);
+      expect(typeof res.body.userID).toEqual('string')
+      expect(typeof res.body.username).toEqual('string')
+    })
+
+    test('Создание пользователя с уже созданной парой с userName и password', async () => {
+      const payload = { "userName" : user.randomUserName(), "password": `${config.defaultPassword}` }
+
+      // шаг с созданием дубликата-юзера до
+      await user.create(payload)
+
+      const res = await user.create(payload)
+
+      console.log(res.body)
+
+      expect(res.status).toEqual(406);
+      expect(res.body.code).toEqual('1204')
+      expect(res.body.message).toEqual('User exists!')
+    })
+  })
+})
