8c2a653784cf0e5d65b819e5214281e9fa9f5f2e
tinkotya <kapibara97@yandex.ru>
2023-10-10 08:01:11 +0300

    дз 7

---
+++ framework/config.js
@@ -0,0 +1,18 @@
+const config = {
+    url: 'https://bookstore.demoqa.com',
+    uuid: '4d949cab-d771-4a44-ba88-7c3e54d4b91a',
+    credentials: {
+        "userName": "tinkotya",
+        "password": "Tinkotya@19071997"
+    },
+    credentialsTestUser: {
+        "userName": "tinkotya_test",
+        "password": "Tinkotya_test@1"
+    },
+    credentialsFalse: {
+        "userName": "qwerty",
+        "password": "Qwerty_test@1"
+    }
+}
+
+export default config;
